4a0ce2ba88cf854e57e82bcfb9997c2cfc732b55
BaleLin <bale.lin@oocl.com>
2018-08-23 19:43:47 +0800

    [bale,quinn]write a repository tdd for find all parkingLots

--- src/test/java/com/refactor/parkingLot/repository/ParkingLotReositoryTest.java
+++ src/test/java/com/refactor/parkingLot/repository/ParkingLotReositoryTest.java
@@ -1,6 +1,7 @@
 package com.refactor.parkingLot.repository;
 
 import com.refactor.parkingLot.entites.ParkingLot;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 
 @RunWith(SpringRunner.class)
@@ -30,4 +34,16 @@ public class ParkingLotReositoryTest {
 
     }
 
+    @Test
+    public void should_query_all_parkingLots_when_call_findAll(){
+        testEntityManager.persist(new ParkingLot("东南停车场",20));
+        testEntityManager.persist(new ParkingLot("西南停车场",20));
+        List<ParkingLot> parkingLotList = parkingLotRepository.findAll();
+        Assert.assertEquals(parkingLotList.size(),2);
+        Assert.assertEquals(parkingLotList.get(0).getName(),"东南停车场");
+        Assert.assertEquals(parkingLotList.get(1).getName(),"西南停车场");
+        Assert.assertEquals(parkingLotList.get(0).getSize(),20);
+        Assert.assertEquals(parkingLotList.get(1).getSize(),20);
+
+    }
 }
